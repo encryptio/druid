208ec7dcec006eb713e8e3cbae7cb0f9949691de
Chris Kastorff <encryptio@gmail.com>
2011-11-23 05:49:17 -0700

    gf_add, gf_mult, and gf_div are now inline functions for 2.5x speed in reed-solomon test

--- src/galois_field.c
+++ src/galois_field.c
@@ -4,43 +4,16 @@
 
 #include "galois_field.h"
 
-#define NW 256
-#define PRIM_POLY 0435
-
-static uint8_t gflog[NW];
-static uint8_t gfilog[NW];
-
-uint8_t gf_add(uint8_t l, uint8_t r) {
-    return l^r;
-}
-
-uint8_t gf_mult(uint8_t l, uint8_t r) {
-    if ( l == 0 || r == 0 )
-        return 0;
-
-    int_fast16_t sum_log = ((int_fast16_t) gflog[l]) + gflog[r];
-    if ( sum_log >= NW-1 ) sum_log -= NW-1;
-
-    return gfilog[sum_log];
-}
-
-uint8_t gf_div(uint8_t l, uint8_t r) {
-    if ( l == 0 ) return 0;
-    if ( r == 0 ) return 0; // divide by zero... call error?
-    
-    int_fast16_t diff_log = ((int_fast16_t) gflog[l]) - gflog[r];
-    if ( diff_log < 0 ) diff_log += NW-1;
-
-    return gfilog[diff_log];
-}
+uint8_t gf_log[GF_NW];
+uint8_t gf_ilog[GF_NW];
 
 void gf_initialize() {
     int_fast16_t b = 1;
-    for (int log = 0; log < NW-1; log++) {
-        gflog[b] = log;
-        gfilog[log] = b;
+    for (int log = 0; log < GF_NW-1; log++) {
+        gf_log[b] = log;
+        gf_ilog[log] = b;
         b <<= 1;
-        if ( b & NW ) b ^= PRIM_POLY;
+        if ( b & GF_NW ) b ^= GF_PRIM_POLY;
     }
 }
 
--- src/galois_field.h
+++ src/galois_field.h
@@ -3,9 +3,36 @@
 
 #include <inttypes.h>
 
-uint8_t gf_add(uint8_t l, uint8_t r);
-uint8_t gf_mult(uint8_t l, uint8_t r);
-uint8_t gf_div(uint8_t l, uint8_t r);
+#define GF_NW 256
+#define GF_PRIM_POLY 0435
+
+extern uint8_t gf_log[GF_NW];
+extern uint8_t gf_ilog[GF_NW];
+
+static inline uint8_t gf_add(uint8_t l, uint8_t r) {
+    return l^r;
+}
+
+static inline uint8_t gf_mult(uint8_t l, uint8_t r) {
+    if ( l == 0 || r == 0 )
+        return 0;
+
+    int_fast16_t sum_log = ((int_fast16_t) gf_log[l]) + gf_log[r];
+    if ( sum_log >= GF_NW-1 ) sum_log -= GF_NW-1;
+
+    return gf_ilog[sum_log];
+}
+
+static inline uint8_t gf_div(uint8_t l, uint8_t r) {
+    if ( l == 0 ) return 0;
+    if ( r == 0 ) return 0; // divide by zero... call error?
+    
+    int_fast16_t diff_log = ((int_fast16_t) gf_log[l]) - gf_log[r];
+    if ( diff_log < 0 ) diff_log += GF_NW-1;
+
+    return gf_ilog[diff_log];
+}
+
 void gf_initialize();
 
 #endif
