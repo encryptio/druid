90dbffb6a50e041b52ec1643660f1d79154861cd
Chris Kastorff <encryptio@gmail.com>
2011-12-07 09:01:40 -0700

    use real CRC-32c as generated by pycrc

--- src/crc.h
+++ src/crc.h
@@ -1,8 +1,100 @@
+
+
+// Modified pycrc output
+
+/**
+ * \file crc.h
+ * Functions and types for CRC checks.
+ *
+ * Generated on Wed Dec  7 08:56:08 2011,
+ * by pycrc v0.7.8, http://www.tty1.net/pycrc/
+ * using the configuration:
+ *    Width        = 32
+ *    Poly         = 0x1edc6f41
+ *    XorIn        = 0xffffffff
+ *    ReflectIn    = True
+ *    XorOut       = 0xffffffff
+ *    ReflectOut   = True
+ *    Algorithm    = table-driven
+ *****************************************************************************/
 #ifndef __CRC_H__
 #define __CRC_H__
 
-#include <inttypes.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-uint32_t calc_crc32(uint8_t *ram, uint64_t len);
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+
+/**
+ * The definition of the used algorithm.
+ *****************************************************************************/
+#define CRC_ALGO_TABLE_DRIVEN 1
+
+
+/**
+ * The type of the CRC values.
+ *
+ * This type must be big enough to contain at least 32 bits.
+ *****************************************************************************/
+typedef uint32_t crc_t;
+
+
+/**
+ * Reflect all bits of a \a data word of \a data_len bytes.
+ *
+ * \param data         The data word to be reflected.
+ * \param data_len     The width of \a data expressed in number of bits.
+ * \return             The reflected data.
+ *****************************************************************************/
+crc_t crc_reflect(crc_t data, size_t data_len);
+
+
+/**
+ * Calculate the initial crc value.
+ *
+ * \return     The initial crc value.
+ *****************************************************************************/
+static inline crc_t crc_init(void)
+{
+    return 0xffffffff;
+}
+
+
+/**
+ * Update the crc value with new data.
+ *
+ * \param crc      The current crc value.
+ * \param data     Pointer to a buffer of \a data_len bytes.
+ * \param data_len Number of bytes in the \a data buffer.
+ * \return         The updated crc value.
+ *****************************************************************************/
+crc_t crc_update(crc_t crc, const unsigned char *data, size_t data_len);
+
+
+/**
+ * Calculate the final crc value.
+ *
+ * \param crc  The current crc value.
+ * \return     The final crc value.
+ *****************************************************************************/
+static inline crc_t crc_finalize(crc_t crc)
+{
+    return crc ^ 0xffffffff;
+}
+
+/**
+ * Wrapper, added manually
+ */
+
+static inline uint32_t calc_crc32(uint8_t *ram, uint64_t len) {
+    return crc_finalize(crc_update(crc_init(), ram, len));
+}
+
+#ifdef __cplusplus
+}           /* closing brace for extern "C" */
+#endif
+
+#endif      /* __CRC_H__ */
