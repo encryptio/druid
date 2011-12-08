e1acc9ea2dfeb0ea0247f1b2c014acf69f517b57
Chris Kastorff <encryptio@gmail.com>
2011-12-08 15:44:51 -0700

    [lua] bind bdev

--- src/lua/raw-bindings.c
+++ src/lua/raw-bindings.c
@@ -36,6 +36,191 @@ static inline void require_exactly(lua_State *L, int ct) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+#include "bdev.h"
+
+static int bind_bdev_get_block_size(lua_State *L) {
+    require_exactly(L, 1);
+
+    if ( !lua_islightuserdata(L, 1) )
+        return luaL_argerror(L, 1, "not a light userdata");
+
+    struct bdev *dev = lua_touserdata(L, 1);
+    lua_pop(L, 1);
+
+    lua_pushnumber(L, dev->block_size);
+
+    return 1;
+}
+
+static int bind_bdev_get_block_count(lua_State *L) {
+    require_exactly(L, 1);
+
+    if ( !lua_islightuserdata(L, 1) )
+        return luaL_argerror(L, 1, "not a light userdata");
+
+    struct bdev *dev = lua_touserdata(L, 1);
+    lua_pop(L, 1);
+
+    lua_pushnumber(L, dev->block_count);
+
+    return 1;
+}
+
+static int bind_bdev_read_bytes(lua_State *L) {
+    require_exactly(L, 3);
+
+    if ( !lua_islightuserdata(L, 1) )
+        return luaL_argerror(L, 1, "not a light userdata");
+
+    struct bdev *dev = lua_touserdata(L, 1);
+    uint64_t start   = luaL_checknumber(L, 2);
+    uint64_t len     = luaL_checknumber(L, 2);
+    lua_pop(L, 3);
+
+    uint8_t *data;
+    if ( (data = malloc(len)) == NULL )
+        err(1, "Couldn't allocate space for data read");
+
+    if ( dev->read_bytes(dev, start, len, data) )
+        lua_pushlstring(L, (char*)data, len);
+    else
+        lua_pushnil(L);
+
+    free(data);
+
+    return 1;
+}
+
+static int bind_bdev_write_bytes(lua_State *L) {
+    require_exactly(L, 3);
+
+    if ( !lua_islightuserdata(L, 1) )
+        return luaL_argerror(L, 1, "not a light userdata");
+
+    struct bdev *dev    = lua_touserdata(L, 1);
+    uint64_t start      = luaL_checknumber(L, 2);
+    size_t len;
+    const uint8_t *data = (const uint8_t *) luaL_checklstring(L, 3, &len);
+
+    bool ret = dev->write_bytes(dev, start, len, data);
+    lua_pop(L, 3);
+
+    lua_pushboolean(L, ret);
+
+    return 1;
+}
+
+static int bind_bdev_read_block(lua_State *L) {
+    require_exactly(L, 2);
+
+    if ( !lua_islightuserdata(L, 1) )
+        return luaL_argerror(L, 1, "not a light userdata");
+
+    struct bdev *dev = lua_touserdata(L, 1);
+    uint64_t which   = luaL_checknumber(L, 2);
+    lua_pop(L, 2);
+
+    uint8_t *data;
+    if ( (data = malloc(dev->block_size)) == NULL )
+        err(1, "Couldn't allocate space for block read");
+
+    if ( dev->read_block(dev, which, data) )
+        lua_pushlstring(L, (char*)data, dev->block_size);
+    else
+        lua_pushnil(L);
+
+    free(data);
+
+    return 1;
+}
+
+static int bind_bdev_write_block(lua_State *L) {
+    require_exactly(L, 3);
+
+    if ( !lua_islightuserdata(L, 1) )
+        return luaL_argerror(L, 1, "not a light userdata");
+
+    struct bdev *dev    = lua_touserdata(L, 1);
+    uint64_t which      = luaL_checknumber(L, 2);
+    size_t len;
+    const uint8_t *data = (const uint8_t *) luaL_checklstring(L, 3, &len);
+
+    if ( len != dev->block_size ) {
+        luaL_where(L, 1);
+        lua_pushstring(L, "Wrong block size (got ");
+        lua_pushnumber(L, len);
+        lua_pushstring(L, " bytes, expected ");
+        lua_pushnumber(L, dev->block_size);
+        lua_pushstring(L, "bytes)");
+        lua_concat(L, 6);
+        lua_error(L);
+    }
+
+    bool ret = dev->write_block(dev, which, data);
+    lua_pop(L, 3);
+
+    lua_pushboolean(L, ret);
+
+    return 1;
+}
+
+static int bind_bdev_close(lua_State *L) {
+    require_exactly(L, 1);
+
+    if ( !lua_islightuserdata(L, 1) )
+        return luaL_argerror(L, 1, "not a light userdata");
+
+    struct bdev *dev = lua_touserdata(L, 1);
+    lua_pop(L, 1);
+
+    dev->close(dev);
+
+    return 0;
+}
+
+static int bind_bdev_clear_caches(lua_State *L) {
+    require_exactly(L, 1);
+
+    if ( !lua_islightuserdata(L, 1) )
+        return luaL_argerror(L, 1, "not a light userdata");
+
+    struct bdev *dev = lua_touserdata(L, 1);
+    lua_pop(L, 1);
+
+    dev->clear_caches(dev);
+
+    return 0;
+}
+
+static int bind_bdev_flush(lua_State *L) {
+    require_exactly(L, 1);
+
+    if ( !lua_islightuserdata(L, 1) )
+        return luaL_argerror(L, 1, "not a light userdata");
+
+    struct bdev *dev = lua_touserdata(L, 1);
+    lua_pop(L, 1);
+
+    dev->flush(dev);
+
+    return 0;
+}
+
+static int bind_bdev_sync(lua_State *L) {
+    require_exactly(L, 1);
+
+    if ( !lua_islightuserdata(L, 1) )
+        return luaL_argerror(L, 1, "not a light userdata");
+
+    struct bdev *dev = lua_touserdata(L, 1);
+    lua_pop(L, 1);
+
+    dev->sync(dev);
+
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 #include "layers/baseio.h"
 
 static int bind_bio_create_malloc(lua_State *L) {
@@ -285,6 +470,17 @@ void bind_druidraw(lua_State *L) {
     lua_pushcfunction(L, bind_##name); \
     lua_settable(L, table); \
 } while (0)
+
+    BIND(bdev_get_block_size);
+    BIND(bdev_get_block_count);
+    BIND(bdev_read_bytes);
+    BIND(bdev_write_bytes);
+    BIND(bdev_read_block);
+    BIND(bdev_write_block);
+    BIND(bdev_close);
+    BIND(bdev_clear_caches);
+    BIND(bdev_flush);
+    BIND(bdev_sync);
 
     BIND(bio_create_malloc);
     BIND(bio_create_mmap);
