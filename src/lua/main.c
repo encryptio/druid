f0edb738ddd8ad3c584ba04990f57f807eef1899
Chris Kastorff <encryptio@gmail.com>
2011-12-09 14:18:47 -0700

    [main] require -i to enter interactive shell when given files

--- src/lua/main.c
+++ src/lua/main.c
@@ -1,6 +1,7 @@
 #include "lua/raw-bindings.h"
 #include "lua/porcelain.h"
 
+#include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -14,6 +15,11 @@
 #include <readline/history.h>
 
 static void interactive_loop(lua_State *L) {
+    if ( !isatty(fileno(stdout)) ) {
+        fprintf(stderr, "Can't open an interactive shell when stdout is not a tty");
+        exit(1);
+    }
+
     char *s;
     while ( (s = readline("druid> ")) != NULL ) {
         add_history(s);
@@ -40,16 +46,32 @@ int main(int argc, char **argv) {
 
     lua_gc(L, LUA_GCCOLLECT, 0);
 
-    for (int i = 1; i < argc; i++)
-        if ( luaL_dofile(L, argv[i]) ) {
+    if ( lua_gettop(L) )
+        lua_pop(L, lua_gettop(L));
+
+    bool run_interactive = false;
+
+    for (int i = 1; i < argc; i++) {
+        if ( strcmp(argv[i], "-i") == 0 ) {
+            run_interactive = true;
+        } else if ( luaL_dofile(L, argv[i]) ) {
             fprintf(stderr, "error executing '%s': %s\n", argv[i], luaL_checkstring(L, -1));
             exit(1);
         }
 
-    lua_gc(L, LUA_GCCOLLECT, 0);
+        if ( lua_gettop(L) )
+            lua_pop(L, lua_gettop(L));
 
-    if ( isatty(fileno(stdout)) )
+        lua_gc(L, LUA_GCCOLLECT, 0);
+    }
+
+    if ( argc == 1 )
+        run_interactive = true;
+
+    if ( run_interactive )
         interactive_loop(L);
+
+    lua_gc(L, LUA_GCCOLLECT, 0);
 
     exit(0);
 }
