1b14a26e65e3df10d85fb9e12c54fa0a81de078c
Chris Kastorff <encryptio@gmail.com>
2011-12-13 14:42:49 -0700

    [lua] add socket "tostring" method

--- src/lua/raw-bindings.c
+++ src/lua/raw-bindings.c
@@ -712,6 +712,34 @@ static int bind_loop_sock_get_port(lua_State *L) {
     return 1;
 }
 
+static int bind_loop_sock_tostring(lua_State *L) {
+    require_exactly(L, 1);
+
+    struct socket_data *sd = luaL_checkudata(L, 1, "druid socket");
+    lua_pop(L, 1);
+
+    lua_pushliteral(L, "tcp(");
+
+    if ( sd->state == SOCKET_CONNECTING )
+        lua_pushliteral(L, "connecting to ");
+    else if ( sd->state == SOCKET_OPEN )
+        lua_pushliteral(L, "connected to ");
+    else if ( sd->state == SOCKET_DESTROYED )
+        lua_pushliteral(L, "finished with ");
+    else
+        errx(1, "bind_loop_sock_tostring had no valid sd->state");
+
+    lua_pushstring(L, sd->host);
+    lua_pushliteral(L, " port ");
+    lua_pushinteger(L, sd->port);
+
+    lua_pushliteral(L, ")");
+
+    lua_concat(L, 6);
+    
+    return 1;
+}
+
 static void bind_loop_read_cb(size_t in_buffer, struct loop_sockhandle *h, void *data) {
     struct socket_data *sd = data;
     lua_State *L = sd->L;
@@ -818,6 +846,10 @@ static int bind_loop_tcp_connect(lua_State *L) {
         lua_pushcfunction(L, bind_loop_sock_gc);
         lua_settable(L, table);
 
+        lua_pushliteral(L, "__tostring");
+        lua_pushcfunction(L, bind_loop_sock_tostring);
+        lua_settable(L, table);
+
         lua_pushliteral(L, "__index");
         if ( luaL_newmetatable(L, "druid socket methods") ) {
             int table = lua_gettop(L);
@@ -836,6 +868,10 @@ static int bind_loop_tcp_connect(lua_State *L) {
 
             lua_pushliteral(L, "get_port");
             lua_pushcfunction(L, bind_loop_sock_get_port);
+            lua_settable(L, table);
+            
+            lua_pushliteral(L, "tostring");
+            lua_pushcfunction(L, bind_loop_sock_tostring);
             lua_settable(L, table);
         }
         lua_settable(L, table);
