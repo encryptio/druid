f64af0ede11bbee9ba6eb29923e7a4e3cbd00283
Chris Kastorff <encryptio@gmail.com>
2011-12-17 16:46:22 -0700

    [lua-examples] revamp nbd-server.lua to use druid.tcp_server

--- lua-examples/nbd-server.lua
+++ lua-examples/nbd-server.lua
@@ -2,9 +2,6 @@ function make_base()
     return druid.ram(1024, 1024*64)
 end
 
--- use LuaSocket
-local socket = require("socket")
-
 -- a few constants used in the protocol
 local NBD_PASSWD = "NBDMAGIC"
 local NBD_INITMAGIC = string.char(0x00,0x00,0x42,0x02, 0x81,0x86,0x12,0x53)
@@ -52,76 +49,137 @@ function unpack_be32(str)
     return num
 end
 
-function handle_client(client, device)
-    client:send(NBD_PASSWD)
-    client:send(NBD_INITMAGIC)
-    client:send(pack_be64(device.size))
+local port = 1234
 
-    local zeroes = ''
-    for i = 1,128 do zeroes = zeroes .. string.char(0) end
-    client:send(zeroes)
+local device = make_base()
+print("Created device of " .. device:block_count() .. " blocks of " .. device:block_size() .. " bytes")
+local devicesize = device:block_count() * device:block_size()
 
-    while true do
-        local magic, message = client:receive(4)
-        if magic == nil then
-            print("read failed: " .. message)
-            return
-        end
-        if magic ~= NBD_REQUEST_MAGIC then
-            print("bad request magic - wanted " .. string.format("%q", NBD_REQUEST_MAGIC) .. ", got " .. string.format("%q", magic))
-            return
-        end
+local states = {}
+local buffers = {}
+local extra = {}
 
-        local cmd = client:receive(4)
-        if cmd == NBD_CMD_DISC then
-            return
-        end
+function reset_state(id)
+    states[id] = 'magic'
+    buffers[id] = ''
+    extra[id] = { handle = nil, from = nil, len = nil }
+end
 
-        local handle = client:receive(8)
+function start_connection(sock)
+    sock:write(NBD_PASSWD)
+    sock:write(NBD_INITMAGIC)
+    sock:write(pack_be64(device:block_size() * device:block_count()))
+    sock:write(string.rep(string.char(0), 128))
+end
 
-        local from = unpack_be64(client:receive(8))
-        local len  = unpack_be32(client:receive(4))
+function nibble(sock, len)
+    local id = sock:id()
+    assert(buffers[id]:len() >= len)
 
-        if len > 128*1024 then
-            print("request length too long - is " .. len)
-            return
-        end
+    local ret = string.sub(buffers[id], 1, len)
+    buffers[id] = string.sub(buffers[id], 1+len)
 
-        if cmd == NBD_CMD_READ then
-            print("read " .. from .. "+" .. len)
-            local res = nil
-            if from + len <= device.size then
-                res = device:read_bytes(from, len)
-                if res == nil then
-                    print("read failed in the underlying device")
-                end
-            else
-                print("oversize read")
-            end
+    return ret
+end
 
-            local r = NBD_RESPONSE_MAGIC
+function check_buffer(sock)
+    local id = sock:id()
+    local l = buffers[id]:len()
+    local s = states[id]
 
-            if res ~= nil then
-                r = r .. RESPONSE_OK
-            else
-                r = r .. RESPONSE_NOK
-            end
-            r = r .. handle
-            if res ~= nil then
-                r = r .. res
-            end
+    --print("socket "..id..": "..s.." with "..l.." bytes in buffer")
 
-            client:send(r)
-
-        elseif cmd == NBD_CMD_WRITE then
-            print("write " .. from .. "+" .. len)
-            local data = client:receive(len)
-            local res = false
-            if from + len > device.size then
-                print("oversize write")
-            elseif data:len() ~= len then
-                print("missized read from socket")
+    if s == 'magic' then
+        if l >= 4 then
+            local magic = nibble(sock, 4)
+            if magic ~= NBD_REQUEST_MAGIC then
+                print("bad magic, closing socket")
+                sock:close()
                 return
+            end
+            states[id] = 'cmd'
+            return check_buffer(sock)
+        end
+    elseif s == 'cmd' then
+        if l >= 4 then
+            local cmd = nibble(sock, 4)
+            if cmd == NBD_CMD_DISC then
+                sock:close()
+                return
+            elseif cmd == NBD_CMD_READ then
+                states[id] = 'read'
+                return check_buffer(sock)
+            elseif cmd == NBD_CMD_WRITE then
+                states[id] = 'write'
+                return check_buffer(sock)
+            else
+                print("bad command, closing socket")
+                sock:close()
+                return
+            end
+        end
+    elseif s == 'read' or s == 'write' then
+        if l >= 20 then
+            extra[id]['handle'] = nibble(sock, 8)
+            extra[id]['from']   = unpack_be64(nibble(sock, 8))
+            extra[id]['len']    = unpack_be32(nibble(sock, 4))
+
+            local from = extra[id]['from']
+            local len  = extra[id]['len']
+
+            if len > 128*1024 then
+                print("request length too long - is " .. len)
+                sock:close()
+                return
+            end
+
+            if s == 'read' then
+                print("read " .. from .. "+" .. len)
+
+                local res = nil
+                if from + len <= devicesize then
+                    res = device:read_bytes(from, len)
+                    if res == nil then
+                        print("read failed in the underlying device")
+                    end
+                else
+                    print("oversize read")
+                end
+
+                local r = NBD_RESPONSE_MAGIC
+
+                if res ~= nil then
+                    r = r .. RESPONSE_OK
+                else
+                    r = r .. RESPONSE_NOK
+                end
+                r = r .. extra[id]['handle']
+                if res ~= nil then
+                    r = r .. res
+                end
+
+                sock:write(r)
+
+                states[id] = 'magic'
+
+            elseif s == 'write' then
+                states[id] = 'writedata'
+            else
+                assert(0)
+            end
+
+            return check_buffer(sock)
+        end
+    elseif s == 'writedata' then
+        if l >= extra[id]['len'] then
+            local len  = extra[id]['len']
+            local from = extra[id]['from']
+            print("write " .. from .. "+" .. len)
+
+            local data = nibble(sock, len)
+            local res = false
+            if from + len > devicesize then
+                print("oversize write")
             else
                 res = device:write_bytes(from, data)
             end
@@ -132,33 +190,42 @@ function handle_client(client, device)
             else
                 r = r .. RESPONSE_NOK
             end
-            r = r .. handle
+            r = r .. extra[id]['handle']
 
-            client:send(r)
-        else
-            print("bad request type")
-            return
+            sock:write(r)
+
+            states[id] = 'magic'
+
+            return check_buffer(sock)
         end
     end
 end
 
--- server and accepting loop
-local server = assert(socket.bind("*", 0))
-local ip, port = server:getsockname()
-
-print("Now listening on " .. ip .. ":" .. port)
-
-local device = make_base()
-
-print("Created device of length " .. device.size)
-
-while true do
-    collectgarbage("collect")
-    local client = server:accept()
-    print("got client")
-    handle_client(client, device)
-    client:close()
-    print("done with client")
-    device:flush()
+-- don't get too far out of sync
+function sync_loop()
+    device:sync()
+    druid.timer(5, sync_loop)
 end
+druid.timer(5, sync_loop)
+
+druid.tcp_listen(port, {
+    read = function (s,r)
+        buffers[s:id()] = buffers[s:id()] .. r
+        check_buffer(s)
+    end,
+    connect = function (s)
+        reset_state(s:id())
+        start_connection(s)
+        print("Got connection")
+    end,
+    eof = function (s)
+        reset_state(s:id())
+    end,
+    error = function (s,e)
+        print("error on socket " .. s .. ": " .. e)
+        reset_state(s:id())
+    end
+})
+
+print("Now listening on port " .. port)
 
